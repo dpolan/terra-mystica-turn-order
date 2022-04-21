eb2d7de3536166c25d9c62c369cf01430c67a844
David González Polán <dpolan@oldboycd.com>
2022-04-22 00:16:52 +0200

    Add data object with the different factions in the game

---
+++ app/scripts/data/Factions.js
@@ -0,0 +1,24 @@
+window.TerraMystica = window.TerraMystica || {};
+
+window.TerraMystica.FACTIONS = [
+  { slug: 'chaos-magicians', name: 'Chaos Magicians' },
+  { slug: 'giants', name: 'Giants' },
+  { slug: 'fakirs', name: 'Fakirs' },
+  { slug: 'nomads', name: 'Nomads' },
+  { slug: 'halflings', name: 'Halflings' },
+  { slug: 'cultists', name: 'Cultists' },
+  { slug: 'alchemists', name: 'Alchemists' },
+  { slug: 'darklings', name: 'Darklings' },
+  { slug: 'mermaids', name: 'Mermaids' },
+  { slug: 'swarmlings', name: 'Swarmlings' },
+  { slug: 'auren', name: 'Auren' },
+  { slug: 'witches', name: 'Witches' },
+  { slug: 'engineers', name: 'Engineers' },
+  { slug: 'dwarves', name: 'Dwarves' },
+  { slug: 'yetis', name: 'Yetis' },
+  { slug: 'ice-maidens', name: 'Ice Maidens' },
+  { slug: 'acolytes', name: 'Acolytes' },
+  { slug: 'dragonlords', name: 'Dragonlords' },
+  { slug: 'shapeshifters', name: 'Shapeshifters' },
+  { slug: 'riverwalkers', name: 'Riverwalkers' },
+];
